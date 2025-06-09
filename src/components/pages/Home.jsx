532dd24508ac048d2e511aa2793910824d3da53f
Arti <tienduangmaiphan1998@gmail.com>
2025-06-09 20:32:56 +0700

    remove newsapi

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { RevealOnScroll } from "../utils/RevealOnScroll";
-import NewBoards from "../sections/NewBoards";
+// import NewBoards from "../sections/NewBoards";
 import {Navbar} from "../sections/Navbar"
 
 export const Home = () => {
@@ -10,7 +10,6 @@ export const Home = () => {
     >
       <RevealOnScroll>
         <Navbar />
-        <NewBoards />
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
             Hi, I'm Duang
