deb4a05f0dee4de64ef18723dd33a20fe488fe83
Arti <tienduangmaiphan1998@gmail.com>
2025-06-15 14:45:15 +0700

    fix register loading section

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,14 +1,25 @@
 import { RevealOnScroll } from "../utils/RevealOnScroll";
-import {Navbar} from "../sections/Navbar"
+import { Navbar } from "../sections/Navbar";
 import { RegisterForm } from "../sections/RegisterForm";
 import { Footer } from "../sections/footer";
+import { useState } from "react";
+import { LoadingScreen } from "../utils/LoadingScreen";
 
 export const Register = () => {
+  const [loading, setLoading] = useState(false);
   return (
     <section
-      id="home"
+      id="register"
       className="min-h-screen flex items-center justify-center relative"
     >
+      {" "}
+      {!loading && (
+        <LoadingScreen
+          onComplete={() => {
+            setLoading(true);
+          }}
+        />
+      )}{" "}
       <RevealOnScroll>
         <Navbar />
         <RegisterForm />
